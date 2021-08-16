13c7f2d23f57c3bf5e24c11af753b348f1ae88c0
sathyakaipa <88481842+sathyakaipa@users.noreply.github.com>
2021-08-16 17:57:48 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FaComponent } from './fa/fa.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FaComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
